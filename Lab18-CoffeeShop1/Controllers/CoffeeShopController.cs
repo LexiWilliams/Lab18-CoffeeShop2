ceda577ee983077e6d18fcea9387a259d453a441
Lexi <lexa.shelby@gmail.com>
2019-08-19 17:05:36 -0400

    lab 18 part 2

---
+++ Lab18-CoffeeShop1/Controllers/CoffeeShopController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_CoffeeShop1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_CoffeeShop1.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+
+            return View(new RegisterUser());
+        }
+        [HttpPost]
+        public IActionResult Registration(RegisterUser newUser)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Welcome", newUser);
+            }
+            return View(newUser);
+        }
+        public IActionResult Welcome(RegisterUser user)
+        {
+            return View(user);
+        }
+      
+        public IActionResult DisplayInfo(RegisterUser user)
+        {
+            ViewBag.FirstName = user.FirstName;
+            ViewBag.LastName = user.LastName;
+            ViewBag.Email = user.Email;
+            ViewBag.Password = user.Password;
+            ViewBag.DOB = user.DOB;
+            ViewBag.Gender = user.Gender;
+            ViewBag.ZipCode = user.ZipCode;
+            return View();
+        }
+    }
+}
---
+++ Lab18-CoffeeShop1/Models/RegisterUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18_CoffeeShop1.Models
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage ="First name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage ="Last name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required"), EmailAddress(ErrorMessage ="Invalid email"),DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required"),DataType(DataType.Password),RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
+            ErrorMessage ="Invalid Password: Must contain 1 capital letter, 1 number, and at least 8 characters")]
+        public string Password { get; set; }
+        [Required(ErrorMessage ="Birthday is required"), DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [StringLength(6)]
+        public string ZipCode { get; set; }
+    }
+}
